bc992c3e316440a8c461bc0295ec7ef1682bf9db
K2365 <K2365@ICT-421-WS-22>
2017-01-16 18:25:54 +0200

    Init commit

--- olio_ja_repository/olio_ja_repository/Program.cs
+++ olio_ja_repository/olio_ja_repository/Program.cs
@@ -2,19 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
-namespace olio_ja_repository
+namespace Hello_Word
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello GitHub!");
-            Console.WriteLine("Voi jumsjams");
-            Console.WriteLine("PASKAA.TXT JEE JEE");
-
-
+            int kerros, Space, Number, i = 1;
+            char merkki = '*';
+            Console.WriteLine("Montako kerrosta?");
+            kerros = int.Parse(Console.ReadLine());
+            for (i = 1; i <= kerros; i++) // Kerrosten määrä
+            {
+                for (Space = 1; Space <= (kerros - i); Space++)  // Välilyönnit jotta saadaan puun muoto
+                    Console.Write(" ");
+                for (Number = 1; Number <= i; Number++) //Vasen puoli
+                    Console.Write(merkki);
+                for (Number = (i - 1); Number >= 1; Number--)  //Oikea puoli
+                    Console.Write(merkki);
+                Console.WriteLine();
+            }
+            //Console.WriteLine(i);
+            string[] tyhjat = new string[kerros];
+            for (int h = 0; h <= i - 3; h++)  //Määrittää montako väliä tulee ennen "runkoa"
+            {
+                tyhjat[h] += " ";
+            }
+            for (int j = 1; j <= 2; j++) //Tulostaa rungon
+            {
+                for (int k = 0; k <= i - 2; k++) //Tulostaa välit runkoon asti
+                {
+                    Console.Write(tyhjat[k]);
+                }
+                Console.WriteLine("*");
+            }
         }
     }
 }
