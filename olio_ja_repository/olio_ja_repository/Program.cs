c66853c3872649543b1a7566f0ae480c533f00e4
Ville Halminen <vhalminen@gmail.com>
2017-01-21 11:55:21 +0200

    Erotetaan aliohjelma pääohjelmasta

--- olio_ja_repository/olio_ja_repository/Program.cs
+++ olio_ja_repository/olio_ja_repository/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Televisioclassi;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,50 +13,20 @@ namespace Hello_Word
     {
         static void Main(string[] args)
         {
-            int x = 10;
-            for (int i = 0; i != x; i++)
-            {
-                Jasenet person1 = new Jasenet();
-                Console.WriteLine("Kerro puheenjohtajan nimi: ");
-                person1.Nimi = Console.ReadLine();
-                Console.WriteLine("Kerro ika: ");
-                person1.Ika = int.Parse(Console.ReadLine());
-                person1.SanoNimi();
-                person1.SanoIka();
-            }
+            Televisio LG = new Televisio("LG", "Black", 45, "1920x1080");
+
+            LG.PrintInfo();
+            LG.SwitchChannel();
+            LG.ChangeVolume(5);
+
+            Console.WriteLine();
+
+            Televisio Samsung = new Televisio("Samsung", 6, 25);
+            Samsung.PrintInfo();
         }
     }
 
 
-    class Jasenet
-    {
-
-        private int ika = 0;
-
-        //Fields
-        public string Nimi { get; set; }
-        public int Ika {
-            get; set;
-        }
-
-
-        //Constructor
-        public Jasenet()
-        {
-        }
-
-
-        //Methods
-        public void SanoNimi()
-        {
-            Console.WriteLine("Nimi on " + Nimi);
-        }
-        public void SanoIka()
-        {
-            Console.WriteLine("Ika on " + Ika);
-        }
-
-    }
 
 
 }
