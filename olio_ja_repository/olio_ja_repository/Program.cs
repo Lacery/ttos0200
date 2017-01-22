e1797680d6d03f1dfe55aa1b4d4d520b7d14b9e0
Ville Halminen <vhalminen@gmail.com>
2017-01-22 19:07:43 +0200

    Change the structure of repo. Add all the tehtävät

--- olio_ja_repository/olio_ja_repository/Program.cs
+++ olio_ja_repository/olio_ja_repository/Program.cs
@@ -1,31 +1,26 @@
 ﻿using System;
-using Vehicles;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-
-
-
+using olio_ja_repository;
 namespace Hello_Word
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Vehicle vehje = new Vehicle("Auto", 120, 4);
-            vehje.PrintData();
-            Console.WriteLine(vehje.ToString());
-
-
-            Vehicle rekka = new Vehicle("rekka", 100, 6);
-            rekka.PrintData();
-            Console.WriteLine(rekka.ToString());
-
+            int nro;
+            Console.WriteLine("Mikä tehtävä ajetaan?");
+            nro = int.Parse(Console.ReadLine());
+            Console.Clear();
+            switch (nro)
+            {
+                default: Console.WriteLine("Ei kelvollinen tehtävänumero. Sammutetaan."); break;
+                case 1: t1.T1(); break;
+                case 2: t2.T2(); break;
+                case 3: t3.T3(); break;
+                case 4: t4.T4(); break;
+                case 5: t5.T5(); break;
+                case 6: t6.T6(); break;
+                case 7: t7.T7(); break;
+            }
         }
     }
-
-
-
-
 }
---
+++ olio_ja_repository/olio_ja_repository/t1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace olio_ja_repository
+{
+
+    public class t1
+    {
+        public static void T1()
+        {
+            int[] luvut = new int[5];
+            int yhteensa;
+
+            for (int i = 0; i <= 4; i++)
+            {
+
+                Console.WriteLine("Anna pisteet: ");
+                luvut[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Pienin pistemaara: " + luvut.Min());
+            Console.WriteLine("Suurin pistemaara: " + luvut.Max());
+
+            int sum = luvut.Sum();
+
+            yhteensa = sum - luvut.Min() - luvut.Max();
+            Console.WriteLine("Kokonaispisteet: " + yhteensa);
+
+
+        }
+    }
+}
