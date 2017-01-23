77ff2e796e0c8b0c493e3c512fd56723dce6c437
K2365 <K2365@ICT-421-WS-22>
2017-01-23 19:08:47 +0200

    Kutonen ja vitonen vähän kyseenalasia, muut bueno

--- olio_ja_repository/olio_ja_repository/Program.cs
+++ olio_ja_repository/olio_ja_repository/Program.cs
@@ -23,6 +23,7 @@ namespace Hello_Word
                 case 3: t3.T3(); break;
                 case 4: t4.T4(); break;
                 case 5: t5.T5(); break;
+                case 6: t6.T6(); break;
             }
         }
     }
---
+++ olio_ja_repository/olio_ja_repository/t6.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace olio_ja_repository
+{
+    public class t6
+    {
+
+        //Fields
+
+
+        //Properties
+        public static string Artist;
+        public static string Name;
+        public static double Price;
+        public static string Genre;
+        public static int NrOfSongs;
+        //Constructors
+        public t6()
+        {
+
+        }
+
+        //Methods
+
+        public static void T6() //"Pääohjelma" jolla ajetaan luokan metodeja
+        {
+            t6 Thriller = new t6();
+            Thriller.PrintInfo("Michael Jackson", "Thriller", 19.99, "Pop", 9);
+
+
+        }
+
+        public void PrintInfo(string artist, string name, double price, string genre, int nrofsongs)
+        {
+            Artist = artist;
+            Name = name;
+            Price = price;
+            Genre = genre;
+            NrOfSongs = nrofsongs;
+            Console.WriteLine("Artist: " + Artist + "\nAlbum name: " + Name + "\nGenre: " + Genre + "\nPrice: " + Price + "\nNumber of songs: " + NrOfSongs);
+        }
+
+
+            
+    }
+}
+
+
+
+
+
