cc3693fd9eb7926474233d0a31a65500fcb458b6
K2365 <K2365@MEDI-426-WS-34>
2017-01-31 10:46:30 +0200

    tehtava 6 tehty

--- olio_ja_repository/olio_ja_repository/t6.cs
+++ olio_ja_repository/olio_ja_repository/t6.cs
@@ -10,9 +10,75 @@ namespace olio_ja_repository
     {
         public static void T6()
         {
+            Disk Levy = new CD(2007, "Victory Songs", 20);
+            Console.WriteLine(Levy.ToString());
 
 
+            Disk BluRay = new BluRay(2005, "Frozen", new string[] { "Eka" , "toka", "kolmas" });
+            Console.WriteLine(BluRay.ToString());
 
         }
     }
+
+    class Disk
+    {
+
+        public string Name { get; set; }
+        public int Year { get; set; }
+
+        public Disk()
+        {
+
+        }
+        public Disk(int year, string name)
+        {
+            Year = year;
+            Name = name;
+        }
+        public virtual string ToString()
+        {
+            return "Name: " + Name + "\nYear: " + Year + "\n";
+        }
+
+
+    }
+
+    class CD : Disk
+    {
+
+        public int SongAmount { get; set; }
+
+        public CD(int year, string name, int songamount) :base(year,name)
+        {
+            SongAmount = songamount;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "Number of songs: " + SongAmount + "\n";
+        }
+
+    }
+
+
+    class BluRay : Disk
+    {
+
+        public string[] Scenes;
+
+        public BluRay(int year, string name, string[] scenes) :base(year,name)
+        {
+            Scenes = scenes;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "Kohtaukset: " + Scenes[0] + Scenes[1] + Scenes[2];
+        }
+
+
+    }
+
+
+
 }
