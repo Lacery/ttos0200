11e5cecd158c8a3ea03c9e4f21509d0a5ea0bf48
K2365 <K2365@MEDI-426-WS-34>
2017-02-06 18:44:32 +0200

    mitä_aktuaalista_vittua_minä_juuri_kuulin

--- olio_ja_repository/olio_ja_repository/t1.cs
+++ olio_ja_repository/olio_ja_repository/t1.cs
@@ -10,25 +10,85 @@ namespace olio_ja_repository
     {
         public static void T1()
         {
-            int[] luvut = new int[5];
-            int yhteensa;
+            Persons Ville = new Persons("Ville", 23, "160494-119M");
 
-            for (int i = 0; i <= 4; i++)
-            {
 
-                Console.WriteLine("Anna pisteet: ");
-                luvut[i] = int.Parse(Console.ReadLine());
-            }
-
-            Console.WriteLine("Pienin pistemaara: " + luvut.Min());
-            Console.WriteLine("Suurin pistemaara: " + luvut.Max());
-
-            int sum = luvut.Sum();
-
-            yhteensa = sum - luvut.Min() - luvut.Max();
-            Console.WriteLine("Kokonaispisteet: " + yhteensa);
+            Ville.AddPerson("Ville", 23, "1601431-123M");
+            Console.WriteLine(Ville.PrintData());
 
 
         }
     }
+
+    class Person
+    {
+        //Person-luokka yksittäisen henkilön tietojen ylläpitämiseen ja tulostamiseen
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string HeTu { get; set; }
+
+        public Person()
+        {
+
+        }
+
+        public Person(string name, int age, string hetu)
+        {
+            Name = name;
+            Age = age;
+            HeTu = hetu;
+        }
+
+
+
+        public virtual string PrintData()
+        {
+            return "ss";
+        }
+
+    }
+
+
+    class Persons : Person
+    {
+        //List-kokoelma yksittäisille henkilöille -> 
+        public List<Person> Lista { get; set; }
+
+        public Persons(string name, int age, string hetu):base(name,age,hetu)
+        {
+        }
+
+
+
+        public void AddPerson(string name, int age, string hetu)
+        {
+            //Lisätään henkilö Listaan
+            Lista.Add(new Person { Name = name, Age = age, HeTu = hetu });
+        }
+
+        public void GetPerson()
+        {
+            //Haetaan henkilö listan alkion indeksillä
+
+        }
+
+        public void FindPerson()
+        {
+            //Haetaan henkilö Listasta hetu:lla
+
+        }
+
+
+        public override string PrintData()
+        {
+
+            //Tulostetaan kaikki Listan henkilöt 
+
+            return Lista[0].Name + " " + Lista[0].Age + " " + Lista[0].HeTu;
+
+        }
+
+    }
+
+
 }
